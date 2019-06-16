2cd62dc01f709104d6ec5385ef8dcd06f617537d
Gustavo Porto <portothree@gmail.com>
2019-06-16 20:20:27 +0100

    install and config multer to upload the image

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const PostController = require('./controllers/PostController');
 
 const routes = new express.Router();
+const upload = multer();
 
-routes.post('/posts', PostController.store);
+routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
