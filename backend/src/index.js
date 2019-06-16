4d35741e7c88b304aa3b28148dd81f0abb80ffb3
Gustavo Porto <portothree@gmail.com>
2019-06-17 00:15:17 +0100

    next guarantee that this middleware will exec but made possible to others routes and middlewares to work too

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,10 @@ const io = require('socket.io')(server);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-app.use((req, res) => {
+app.use((req, res, next) => {
     req.io = io;
+
+    next();
 });
 
 app.use(cors());
