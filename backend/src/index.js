ade057aea60c0e8c1838d68ba8ca4987e172af62
Gustavo Porto <portothree@gmail.com>
2019-06-17 00:06:59 +0100

    use cors to make possible all applications acess the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ require('dotenv').config({ path: '.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
