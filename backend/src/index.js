b61b80c4aa4c7c473acabf79bd42d540d8ebc2d8
Gustavo Porto <portothree@gmail.com>
2019-06-17 00:01:38 +0100

    be able to acess /files

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,13 @@ require('dotenv').config({ path: '.env' });
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
 app.use(require('./routes'));
 
