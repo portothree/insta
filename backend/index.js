17323c2ac6f8dc716ca0cfa831d744ed1d13a7f2
Gustavo Porto <portothree@gmail.com>
2019-06-16 19:07:48 +0100

    config dotenv and mongo uri

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config({ path: '.env' }); 
+
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
     return res.send('InstaPorto')
