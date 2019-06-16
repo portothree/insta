e805672482ff339d00417832875c1f99ccb780f5
Gustavo Porto <portothree@gmail.com>
2019-06-17 00:13:02 +0100

    now server accepts http and websocket connections

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,13 @@ const cors = require('cors');
 const app = express();
 
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+app.use((req, res) => {
+    req.io = io;
+});
 
 app.use(cors());
 
