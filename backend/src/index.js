55396b1bb781bb04a5efb566ca884080ce4b3100
Gustavo Porto <portothree@gmail.com>
2019-06-16 19:58:57 +0100

    config routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,7 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-app.get('/', (req, res) => {
-    return res.send('InstaPorto')
-});
+app.use(require('./routes'));
 
 app.listen(3333, function () {
     console.log('App listening port 3333')
