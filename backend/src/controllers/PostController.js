9fa3d9a56d1c0c0c12c92bfa273e0a2132444f88
Gustavo Porto <portothree@gmail.com>
2019-06-17 00:04:52 +0100

    fix to save images with .jpg extension

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -15,11 +15,14 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
+
         await sharp(req.file.path)
             .resize(500)
             .jpeg({ quality: 70 })
             .toFile(
-                path.resolve(req.file.destination, 'resized', image)
+                path.resolve(req.file.destination, 'resized', fileName)
             )
 
         
@@ -30,7 +33,7 @@ module.exports = {
             place,
             description,
             hashtags,
-            image
+            image: fileName,
         });
 
         return res.json(post);
