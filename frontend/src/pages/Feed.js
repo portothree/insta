ce935b81968c2d87a250607e602fa35642665512
Gustavo Porto <portothree@gmail.com>
2019-06-17 14:27:47 +0100

    create the Feed structure

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+class Feed extends Component {
+  render() {
+    return (
+        <section id="post-list">
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span>Autor do post</span>
+                        <span className="place">Local</span>
+                    </div>
+
+                    <img src={more} alt="Mais" />
+                </header>
+
+                <img src='http://localhost:3333/files/example.jpg' alt="" />
+
+                <footer>
+                    <div className="actions">
+                        <button>
+                            <img src={like} alt="" />
+                        </button>
+
+                        <img src={comment} alt="" />
+                        <img src={send} alt="" />
+                    </div>
+
+                    <strong>500 Curtidas</strong>
+
+                    <p>
+                        Descrição
+                        <span>#iLikeIt</span>
+                    </p>
+                </footer>
+
+            </article>
+
+        </section>
+    );
+  }
+}
+
+export default Feed;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Feed from './pages/Feed';
+
 function Routes() {
     return (
         <Switch>
-            <Route path="/" component={} />
+            <Route path="/" exact component={Feed} />
             <Route path="/new" component={}  />
         </Switch>
     );
